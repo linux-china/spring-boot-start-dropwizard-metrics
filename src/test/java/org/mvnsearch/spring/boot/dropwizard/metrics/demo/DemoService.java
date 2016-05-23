7eb111d9bbca5d39ae48ada33df30c60b4e6b245
linux_china <linux_china@hotmail.com>
2016-05-23 22:01:21 +0800

    Demo Service

---
+++ src/test/java/org/mvnsearch/spring/boot/dropwizard/metrics/demo/DemoService.java
@@ -0,0 +1,11 @@
+package org.mvnsearch.spring.boot.dropwizard.metrics.demo;
+
+/**
+ * Demo Service
+ *
+ * @author linux_china
+ */
+public interface DemoService {
+
+    public String getName();
+}
