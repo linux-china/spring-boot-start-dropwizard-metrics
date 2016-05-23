73714b998c8d892f86308f07d2001adb4168b05a
linux_china <linux_china@hotmail.com>
2016-05-23 21:42:24 +0800

    metrics demo application

---
+++ src/test/java/org/mvnsearch/spring/boot/dropwizard/metrics/demo/MetricsDemoApplication.java
@@ -0,0 +1,12 @@
+package org.mvnsearch.spring.boot.dropwizard.metrics.demo;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * metrics demo application
+ *
+ * @author linux_china
+ */
+@SpringBootApplication
+public class MetricsDemoApplication {
+}
