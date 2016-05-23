6dcabfab7a5304c594ac45edb1db5aa93b0e4148
linux_china <linux_china@hotmail.com>
2016-05-23 21:42:02 +0800

    demo application test

---
+++ src/test/java/org/mvnsearch/spring/boot/dropwizard/metrics/demo/DemoServiceImpl.java
@@ -0,0 +1,24 @@
+package org.mvnsearch.spring.boot.dropwizard.metrics.demo;
+
+import com.codahale.metrics.Histogram;
+import com.codahale.metrics.annotation.Counted;
+import com.codahale.metrics.annotation.Metric;
+import org.springframework.stereotype.Component;
+
+/**
+ * demo application test
+ *
+ * @author linux_china
+ */
+@Component
+public class DemoServiceImpl implements DemoService {
+    @Metric(name = "histogram-1")
+    private Histogram histogram;
+
+    @Counted(name = "getName")
+    public String getName() {
+        histogram.update(1);
+        return "jacky";
+    }
+
+}
